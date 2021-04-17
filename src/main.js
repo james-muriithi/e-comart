1f53a9a305b598c391b51f1a2670a00d5dc41bd0
James Muriithi <muriithijames556@gmail.com>
2021-04-17 15:42:04 +0300

    project setup

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import { createApp } from 'vue'
+import store from '../store'
 import App from './App.vue'
 import router from './routes/router'
 
+//css
+
 const app = createApp(App)
 
+app.use(store);
 app.use(router);
 
 app.mount('#app')
---
+++ store/index.js
@@ -0,0 +1,7 @@
+import { createStore } from "vuex";
+
+const store = createStore({
+    modules: {},
+});
+
+export default store;
