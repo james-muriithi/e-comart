12d44a5e32f757fa4d4376d361d946c928ae2ccb
James Muriithi <muriithijames556@gmail.com>
2021-04-19 00:48:19 +0300

    added firebase firestore

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/app";
+
+import "firebase/firestore";
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyBgoWKSYqCNqG82BkYm8ogLzJs0_adDV5c",
+  authDomain: "e-comart.firebaseapp.com",
+  projectId: "e-comart",
+  storageBucket: "e-comart.appspot.com",
+  messagingSenderId: "758831920732",
+  appId: "1:758831920732:web:4c9a7e67426e1a3c271899",
+  measurementId: "G-LDPFDBFXBY",
+};
+
+// Initialize Firebase
+export default firebase.initializeApp(firebaseConfig);
