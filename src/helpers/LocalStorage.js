5d4f6d89be58951927f79b0638e54032c8cd81c6
James Muriithi <muriithijames556@gmail.com>
2021-04-19 23:42:04 +0300

    local storage helper

---
+++ src/helpers/LocalStorage.js
@@ -0,0 +1,25 @@
+const saveToStorage = (name, data, json = false) => {
+    if (json) {
+        data = JSON.stringify(data)
+    }
+    localStorage.setItem(name, data);
+}
+
+const getFromStorage = (name, parse=false) => {
+    let data = localStorage.getItem(name);
+    if (parse) {
+        data = JSON.parse(data)
+    }
+
+    return data;
+}
+
+const deleteFromStorage = (name) => {
+    localStorage.removeItem(name)
+}
+
+export {
+    saveToStorage,
+    getFromStorage,
+    deleteFromStorage
+}
