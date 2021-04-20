d71a9ca4a544885287755d57e2454d945c78b0b3
James Muriithi <muriithijames556@gmail.com>
2021-04-21 01:48:47 +0300

    product card

--- src/components/products/ProductCard.vue
+++ src/components/products/ProductCard.vue
@@ -3,17 +3,24 @@
     <div class="product-card">
       <figure class="product-media">
         <div class="product-label-group">
-          <label class="product-label label-new">new</label
-          ><label class="product-label label-off">-10%</label>
+          <label
+            :class="`product-label ${label.type}`"
+            v-for="label in labels"
+            :key="label.name"
+            >{{ label.title }}</label
+          >
         </div>
         <a class="product-image" href="product-single.html"
           ><img :src="productImage" alt="product"
         /></a>
       </figure>
       <div class="product-content">
-        <h5 class="product-price"><span>$25 </span><del>$42</del></h5>
+        <h5 class="product-price">
+          <span>{{ newPrice }}</span
+          ><del>{{ oldPrice }}</del>
+        </h5>
         <h5 class="product-name">
-          <a href="product-single.html">product name</a>
+          <a href="product-single.html">{{ name }}</a>
         </h5>
         <div class="product-action-group">
           <div class="product-action">
@@ -51,10 +58,23 @@
 
 <script>
 export default {
-    computed:{
-        productImage(){
-            return require('../../assets/images/products/01.jpg')
-        }
-    }
-}
+  data() {
+    return {
+      name: "Samsung Note 1",
+      newPrice: "$25",
+      oldPrice: "$42",
+      productImage: require("../../assets/images/products/01.jpg"),
+      labels: [
+        {
+          title: "new",
+          type: "label-new",
+        },
+        {
+          title: "-10%",
+          type: "label-off",
+        },
+      ],
+    };
+  },
+};
 </script>
