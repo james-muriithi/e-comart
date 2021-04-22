d0bc87b4f9218685bedc510ab0baf62bd77b24a1
James Muriithi <muriithijames556@gmail.com>
2021-04-23 01:51:05 +0300

    banner responsivity

--- src/components/promo/PromoSlider.vue
+++ src/components/promo/PromoSlider.vue
@@ -22,41 +22,41 @@ export default {
           id: 1,
           url: "#",
           image: require("../../assets/images/offer1.jpg"),
-          title: "promo"
+          title: "promo",
         },
         {
           id: 2,
           url: "#",
           image: require("../../assets/images/offer2.jpg"),
-          title: "promo"
+          title: "promo",
         },
         {
           id: 3,
           url: "#",
           image: require("../../assets/images/offer3.jpg"),
-          title: "promo"
+          title: "promo",
         },
         {
           id: 4,
           url: "#",
           image: require("../../assets/images/offer4.jpg"),
-          title: "promo"
+          title: "promo",
         },
         {
           id: 4,
           url: "#",
           image:
             "https://mironmahmud.com/ecomart/assets/ltr/images/offer/02.jpg",
-          title: "promo"
+          title: "promo",
         },
         {
           id: 4,
           url: "#",
           image:
             "https://mironmahmud.com/ecomart/assets/ltr/images/offer/11.jpg",
-          title: "promo"
-        }
-      ]
+          title: "promo",
+        },
+      ],
     };
   },
   mounted() {
@@ -73,14 +73,14 @@ export default {
       responsive: [
         {
           breakpoint: 1200,
-          settings: { slidesToShow: 3, slidesToScroll: 1 }
+          settings: { slidesToShow: 3, slidesToScroll: 1 },
         },
         { breakpoint: 992, settings: { slidesToShow: 2, slidesToScroll: 1 } },
         { breakpoint: 768, settings: { slidesToShow: 2, slidesToScroll: 1 } },
-        { breakpoint: 576, settings: { slidesToShow: 1, slidesToScroll: 1 } }
-      ]
+        { breakpoint: 576, settings: { slidesToShow: 1, slidesToScroll: 1 } },
+      ],
     });
-  }
+  },
 };
 </script>
 
@@ -92,6 +92,30 @@ export default {
 @media screen and (max-width: 767px) {
   .promo-slider img {
     height: 114px;
+  }
+}
+
+@media screen and (max-width: 576px) {
+  .promo-slider img {
+    height: 211px;
+  }
+
+  @media screen and (max-width: 456px) {
+    .promo-slider img {
+      height: 190px;
+    }
+  }
+
+  @media screen and (max-width: 406px) {
+    .promo-slider img {
+      height: 170px;
+    }
+  }
+
+  @media screen and (max-width: 356px) {
+    .promo-slider img {
+      height: 140px;
+    }
   }
 }
 
