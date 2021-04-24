e40e4930012f033eaf183d87e73ef9e9b3dcfac4
James Muriithi <muriithijames556@gmail.com>
2021-04-24 23:54:06 +0300

    checkout page require auth

--- src/routes/router.js
+++ src/routes/router.js
@@ -6,6 +6,7 @@ const Login = () => import("../pages/auth/Login.vue");
 const ProductDetails = () => import('../pages/ProductDetails.vue');
 const Checkout = () => import('../pages/Checkout.vue');
 // import ProductDetails from "../pages/ProductDetails.vue";
+import store from '../store/index.js';
 
 const router = createRouter({
   history: createWebHistory(),
@@ -24,7 +25,8 @@ const router = createRouter({
     },
     {
       path: "/checkout",
-      component: Checkout
+      component: Checkout,
+      meta: {requiresAuth: true}
     },
     {
       path: "/product/:id",
@@ -34,5 +36,14 @@ const router = createRouter({
 
   ]
 });
+
+router.beforeEach((to, _, next) => {
+  if (to.meta.requiresAuth && !store.getters.user) {
+    const path = to.path.replace('/', '');
+    next(`/login?redirect=${path}`);
+  }  else {
+    next();
+  }
+})
 
 export default router;
