3d7120c5fe86a46e3a738754c4a37eb147cfea51
James Muriithi <muriithijames556@gmail.com>
2021-06-30 15:19:47 +0300

    updated vue pwa config

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,22 @@
 module.exports = {
-  chainWebpack: config => {
-    config.plugin("html").tap(args => {
+  chainWebpack: (config) => {
+    config.plugin("html").tap((args) => {
       args[0].title = "Ecomart - Home";
       return args;
     });
   },
   configureWebpack: {
     externals: {
-      jquery: "jQuery"
-    }
+      jquery: "jQuery",
+    },
   },
   pwa: {
     name: "Ecomart",
     themeColor: "#5f1fad",
     assetsVersion: "3",
-    manifestOptions: {  }
-  }
+    manifestOptions: {},
+    workboxOptions: {
+      skipWaiting: true,
+    },
+  },
 };
